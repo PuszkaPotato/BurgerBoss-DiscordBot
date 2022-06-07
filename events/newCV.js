fd62c421df304b258405e89322d9230796bd623a
CanExiOne <canexione@gmail.com>
2022-06-07 23:15:43 +0200

    Updated packages and better handling of CVs

--- events/newCV.js
+++ events/newCV.js
@@ -6,20 +6,31 @@ module.exports = {
     execute(message) {
             if(message.channelId === '970267803762458674')
             {
-                console.log(message);
-                const firstAttachment = [...message.attachments][0];
+
+                if(message.attachments.size > 0)
+                {
+                    const firstAttachment = [...message.attachments][0];
                 
-                const attachment = firstAttachment[1].attachment;
+                    var attachment = firstAttachment[1];
+                }
+                
                 if (!message.author.bot) {
                     const embed = new MessageEmbed()
                         .setColor('#3da324')
                         .setTitle(`CV - ${message.author.username}`)
                         .setAuthor({ name: message.author.username, iconURL: `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.jpg` })
                         .setDescription(message.content)
-                        .addField('Załączone CV', '\u200B')
-                        .setImage(attachment)
                         .setTimestamp()
                         .setFooter({ text: 'CV Kandydata BurgerShot'})
+
+                    if(typeof attachment !== 'undefined' && attachment)
+                    {
+                        if(attachment.contentType == 'image/png' || 'image/jpg' || 'image/jpeg')
+                        {
+                            embed.addField('Załączone CV', '\u200B')
+                            embed.setImage(attachment.attachment)
+                        }
+                    }
 
                     channel = message.guild.channels.cache.get('982368446929059881');
 
