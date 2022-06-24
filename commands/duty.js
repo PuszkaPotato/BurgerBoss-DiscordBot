2f63a1068495c3dcc2772d819d156526b4b250ea
CanExiOne <canexione@gmail.com>
2022-06-24 09:59:56 +0200

    Update dependencies and add Licence

--- commands/duty.js
+++ commands/duty.js
@@ -1,3 +1,25 @@
+/**
+ * BurgerBoss is a Discord Bot to help manage a business on FiveM RP server
+ *  Copyright (C) 2022 - Michał Kołodziej (CanExiOne)
+ * 
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License v3 as published by
+ *   the Free Software Foundation.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * 	You can contact original owner of this software on github or by e-mail
+ * 
+ *  @github https://github.com/CanExiOne
+ * 	@email canexione@gmail.com
+ */
+
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
--- database.js
+++ database.js
@@ -1,16 +1,51 @@
-const Sequelize = require('sequelize');
+/**
+ * Discord Bot to help manage a business on FiveM RP server
+ *  Copyright (C) 2022 - Michał Kołodziej (CanExiOne)
+ * 
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License v3 as published by
+ *   the Free Software Foundation.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * 	You can contact original owner of this software on github or by e-mail
+ * 
+ *  @github https://github.com/CanExiOne
+ * 	@email canexione@gmail.com
+ */
 
-const sequelize = new Sequelize('burgerboss', 'burgerboss', 'password' , {
-    host: 'localhost',
-    dialect: 'sqlite',
-    logging: false,
-    storage: 'burgerboss.sqlite',
+const Sequelize = require('sequelize');
+const { DB } = require('./config.json');
+
+const sequelize = new Sequelize(DB.database, DB.user, DB.password , {
+    host: DB.host,
+    dialect: DB.dialect,
+    storage: DB.storage,
+    logging: DB.logging,
     logging: function (str) {
-        console.log(`Sequelize: ${str}`);
+        if(DB.loggingDev)
+        {
+            console.log(`Sequelize: ${str}`);
+        }
     }
 });
 
 module.exports = {
+    Guilds : sequelize.define('guilds', {
+        guildId : {
+            type: Sequelize.INTEGER,
+            unique: true
+        },
+        channels : {
+            type: Sequelize.JSON
+        }
+    }),
     Employees : sequelize.define('employees', {
         name: {
             type: Sequelize.STRING,
